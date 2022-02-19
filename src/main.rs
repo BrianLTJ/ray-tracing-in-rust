fbf6683be50e1479db0ecf961f9fc849cc0a99db
Tingjun Li <BrianLTJ@users.noreply.github.com>
2022-02-20 00:26:09 +0800

    feat: ch2 add eprint

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ fn main() {
     let image_height = 256;
     println!("P3\n{} {}\n255", image_width, image_height);
     for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {}\r", j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width-1) as f64;
-            let g = j as f64 / (image_height-1) as f64;
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
             let b: f64 = 0.25;
 
             let ir = (255.999 * r) as i32;
@@ -14,4 +15,5 @@ fn main() {
             println!("{} {} {}", ir, ig, ib)
         }
     }
+    eprint!("\nDone.\n");
 }
