6dfd55912b86c202310324f67245135e55d1d0ff
Tingjun Li <BrianLTJ@users.noreply.github.com>
2022-02-20 01:21:32 +0800

    feat: use mod

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-fn main() {
-    let image_width = 256;
-    let image_height = 256;
-    println!("P3\n{} {}\n255", image_width, image_height);
-    for j in (0..image_height).rev() {
-        eprint!("\rScanlines remaining: {}\r", j);
-        for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b: f64 = 0.25;
+extern crate raytracing;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-            println!("{} {} {}", ir, ig, ib)
-        }
-    }
-    eprint!("\nDone.\n");
+fn main() {
+    raytracing::ch1::ch1::draw();
 }
